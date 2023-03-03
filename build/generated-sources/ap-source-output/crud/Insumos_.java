b14615b2f432dfab7c0ae122e777b18941e4f8cd
ICam99 <81136921+ICam99@users.noreply.github.com>
2023-03-03 10:39:00 -0700

    xd

--- build/generated-sources/ap-source-output/crud/Insumos_.java
+++ build/generated-sources/ap-source-output/crud/Insumos_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-09T16:58:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-12-09T17:58:47")
 @StaticMetamodel(Insumos.class)
 public class Insumos_ { 
 
